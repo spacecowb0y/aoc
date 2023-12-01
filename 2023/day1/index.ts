d6b148c1941df5162b9fccc0395d5ce93be00c66
Diego <diego.peralta.dev@gmail.com>
2023-12-01 12:37:52 +0100

    refactor(2023): day 1 use fetch to get input

--- 2023/day1/index.ts
+++ 2023/day1/index.ts
@@ -1,28 +1,47 @@
-import { readFileSync } from "fs";
+interface Solutions {
+  part1: number;
+  part2: number;
+}
 
-const input: string[] = readFileSync("input", "utf-8").split("\n");
+const solve = async (): Promise<{ solutions: Solutions }> => {
+  const input: string[] = (
+    await fetch(`https://adventofcode.com/2023/day/1/input`, {
+      headers: {
+        cookie: `session=${process.env.SESSION_TOKEN}`,
+      },
+    }).then((res) => res.text())
+  ).match(/(\S*)\n/g) || [];
 
-const part1: number = input
-  .map((line: string) => line.split("").map(Number).filter(Boolean))
-  .reduce(
-    (sum: number, arr: number[]) => (sum += +`${arr[0]}${arr[arr.length - 1]}`),
-    0
-  );
+  const part1 = (input: string[]): number =>
+    input
+      .map((line) => line.split("").map(Number).filter(Boolean))
+      .reduce((sum, arr) => (sum += +`${arr[0]}${arr[arr.length - 1]}`), 0);
 
-const digits: number[] = input.map((line: string) =>
-  ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
-    .reduce(
-      (acc: string, word: string, index: number) =>
-        acc.replaceAll(word, word + (index + 1) + word),
-      line
-    )
-    .split("")
-    .map(Number)
-    .filter(Boolean)
-    .map((_: number, i: number, arr: number[]) => +`${arr[0]}${arr[arr.length - 1]}`)
-    .reduce((_: number, value: number) => value)
-);
+  const part2 = (input: string[]): number =>
+    input
+      .map((line) =>
+        ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+          .reduce(
+            (acc, word, index) =>
+              acc.replaceAll(word, word + (index + 1) + word),
+            line
+          )
+          .split("")
+          .map(Number)
+          .filter(Boolean)
+          .map((_, i, arr) => +`${arr[0]}${arr[arr.length - 1]}`)
+          .reduce((_, value) => value)
+      )
+      .reduce((sum, value) => sum + value, 0);
 
-const part2: number = digits.reduce((sum: number, value: number) => sum + value, 0);
+  return {
+    solutions: {
+      part1: part1(input),
+      part2: part2(input),
+    },
+  };
+};
 
-console.log({ part1, part2 });
+const solutions = await solve();
+
+console.log(solutions);
